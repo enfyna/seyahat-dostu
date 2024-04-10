89e28216e474b12a7e0f0348cdc39d1ac48d275c
Yunus Efe Yılmaz <yunusefeyilmaz@outlook.com>
2024-04-10 16:56:45 +0300

    Kullanıcı ve Yolculuk için CRUD işlemleri ve bazı özellikleri eklendi.

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -13,7 +13,16 @@ module.exports = {
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
 
-
+    username: { type: 'string', required: true, unique: true },
+    name: { type: 'string', required: true },
+    phone: { type: 'string', required: true },
+    password: { type: 'string', required: true },
+    email: { type: 'string', required: true, unique: true },
+    rides: {
+      collection: 'ride',
+      via: 'users',
+      through: 'rideuser'
+    },
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
     //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
