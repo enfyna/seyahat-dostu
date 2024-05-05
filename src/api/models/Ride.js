838b93ddea1f4247f1d9c59937e0eeb4672cd06c
enfyna <legendraslan@gmail.com>
2024-05-05 12:10:37 +0300

    Model isimlerini ingilizceye çevir

--- src/api/models/Ride.js
+++ src/api/models/Ride.js
@@ -1,5 +1,5 @@
 /**
- * Kullanici.js
+ * Ride.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -13,46 +13,97 @@ module.exports = {
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
 
-    Interests: {
+    Departure_Point: {
       type: 'string',
       required: true
     },
 
-    Driver: {
-      type: 'boolean',
-      required: true
-    },
-
-    Photograph: {
-      type: 'string', // ??
-      required: true
-    },
-
-    Age: {
+    Arrival_Point: {
       type: 'string',
       required: true
     },
 
-    Location: {
+    Date: {
       type: 'string',
       required: true
     },
 
-    Smoking: {
-      type: 'boolean',
-      required: true
-    },
 
-    Point: {
+    Price: {
       type: 'float',
       required: true
     },
 
-    About: {
+
+    Number_of_Ppl: {
+      type: 'int',
+      required: true
+    },
+
+
+    Departure_Time: {
       type: 'string',
       required: true
     },
 
+
+    Arrival_Time: {
+      type: 'string',
+      required: true
+    },
+
+
+    Time: {
+      type: 'string',
+      required: true
+    },
+
+
+    Driver_Name: {
+      type: 'string',
+      required: true
+    },
+
+
+    Driver_Photo: {
+      type: 'string', //?
+      required: true
+    },
+
+
+    Driver_ID: {
+      type: 'string',
+      required: true
+    },
+
+    Driver_Point: {
+      type: 'float',
+      required: true
+    },
+
+    Car_Brand: {
+      type: 'string',
+      required: true
+    },
+
+    Car_Model: {
+      type: 'string',
+      required: true
+    },
+
+
+    Car_Plate: {
+      type: 'string',
+      required: true
+    },
+
+
+    Message: {
+      type: 'string',
+      required: true
+    },
+
+    // ŞÖFÖRÜN ID BİLGİSİ ALINIP DİREK CLASS ÇEKİLEBİLİR Mİ? DENENECEK
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,5 +1,5 @@
 /**
- * Seyahat.js
+ * User.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -13,97 +13,46 @@ module.exports = {
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
 
-    Departure_Point: {
+    Interests: {
       type: 'string',
       required: true
     },
 
-    Arrival_Point: {
+    Driver: {
+      type: 'boolean',
+      required: true
+    },
+
+    Photograph: {
+      type: 'string', // ??
+      required: true
+    },
+
+    Age: {
       type: 'string',
       required: true
     },
 
-    Date: {
+    Location: {
       type: 'string',
       required: true
     },
 
-    
-    Price: {
+    Smoking: {
+      type: 'boolean',
+      required: true
+    },
+
+    Point: {
       type: 'float',
       required: true
     },
 
-    
-    Number_of_Ppl: {
-      type: 'int',
-      required: true
-    },
-
-    
-    Departure_Time: {
+    About: {
       type: 'string',
       required: true
     },
 
-    
-    Arrival_Time: {
-      type: 'string',
-      required: true
-    },
-
-    
-    Time: {
-      type: 'string',
-      required: true
-    },
-
-    
-    Driver_Name: {
-      type: 'string',
-      required: true
-    },
-
-    
-    Driver_Photo: {
-      type: 'string', //?
-      required: true
-    },
-
-    
-    Driver_ID: {
-      type: 'string',
-      required: true
-    },
-
-    Driver_Point: {
-      type: 'float',
-      required: true
-    },
-
-    Car_Brand: {
-      type: 'string',
-      required: true
-    },
-    
-    Car_Model: {
-      type: 'string',
-      required: true
-    },
-
-    
-    Car_Plate: {
-      type: 'string',
-      required: true
-    },
-
-    
-    Message: {
-      type: 'string',
-      required: true
-    },
-
-    // ŞÖFÖRÜN ID BİLGİSİ ALINIP DİREK CLASS ÇEKİLEBİLİR Mİ? DENENECEK
 
     //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
     //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
