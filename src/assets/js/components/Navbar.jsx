07423798845e6d9a7c3d57ca214b443b8c15e662
enfyna <legendraslan@gmail.com>
2024-06-11 14:29:51 +0300

    Navbar menuye kullanıcıyı ilet

--- src/assets/js/components/Navbar.jsx
+++ src/assets/js/components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             {loggedInUser &&
-              <NavbarKullaniciMenu></NavbarKullaniciMenu>
+              <NavbarKullaniciMenu user={loggedInUser}></NavbarKullaniciMenu>
             }
           </div>
         </div>
